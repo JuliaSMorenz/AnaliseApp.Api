377479979d9c24f09c159a5f0859fe927cd359c1
JuliaSMorenz <juliamorenz@gmail.com>
2024-05-25 16:12:05 -0300

    first commit

---
+++ AnaliseApp.Infra.Data/Financeiro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnaliseApp.Infra.Data
+{
+    public class Financeiro
+    {
+        public int Id { get; set; }
+        public Guid EmpresaId { get; set; }
+        public int PessoaId { get; set; }
+        public string? Descricao { get; set; }
+        public DateTime DataEmissao { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
---
+++ AnaliseApp.Infra.Data/Repository.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnaliseApp.Infra.Data
+{
+    public class Repository
+    {
+        private string _connectionString = "Data Source=hackathoncoti.database.windows.net;Initial Catalog=arcaclouddev-26cd5c5d06a3442be89b08db1f72fcf7;Persist Security Info=True;User ID=giom;Password=hackathon@2024;Encrypt=False";
+
+
+        public Financeiro? GetById(Guid PessoaId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT * FROM Financeiro WHERE PessoaId = @PessoaId";
+                return connection.QueryFirstOrDefault<Financeiro>(query, new { PessoaId });
+            }
+        }
+
+        public List<Financeiro> Get(DateTime dataInicio, DateTime dataFim)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                string query = @"SELECT * 
+                                FROM Financeiro 
+                                WHERE DataEmissao >= @DataInicio AND DataEmissao <= @DataFim
+                                ORDER BY DataEmissao DESC";
+
+                return connection.Query<Financeiro>(query, new { DataInicio = dataInicio, DataFim = dataFim }).ToList();
+            }
+        }
+
+    }
+}
