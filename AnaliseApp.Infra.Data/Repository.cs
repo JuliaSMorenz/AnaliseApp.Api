1ece8e444029c7af32b947cbb704e908b09ad451
Julia Morenz <99999279+JuliaSMorenz@users.noreply.github.com>
2024-05-25 17:19:02 -0300

    Add files via upload

--- AnaliseApp.Infra.Data/Repository.cs
+++ AnaliseApp.Infra.Data/Repository.cs
@@ -12,23 +12,13 @@ namespace AnaliseApp.Infra.Data
     {
         private string _connectionString = "Data Source=hackathoncoti.database.windows.net;Initial Catalog=arcaclouddev-26cd5c5d06a3442be89b08db1f72fcf7;Persist Security Info=True;User ID=giom;Password=hackathon@2024;Encrypt=False";
 
-
-        public Financeiro? GetById(Guid PessoaId)
-        {
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                string query = "SELECT * FROM Financeiro WHERE PessoaId = @PessoaId";
-                return connection.QueryFirstOrDefault<Financeiro>(query, new { PessoaId });
-            }
-        }
-
-        public List<Financeiro> Get(DateTime dataInicio, DateTime dataFim)
+        public List<Financeiro> Get(DateTime dataInicio, DateTime dataFim, Guid PessoaId)
         {
             using (var connection = new SqlConnection(_connectionString))
             {
                 string query = @"SELECT * 
                                 FROM Financeiro 
-                                WHERE DataEmissao >= @DataInicio AND DataEmissao <= @DataFim
+                                WHERE DataEmissao >= @DataInicio AND DataEmissao <= @DataFim AND PessoaId = @PessoaId
                                 ORDER BY DataEmissao DESC";
 
                 return connection.Query<Financeiro>(query, new { DataInicio = dataInicio, DataFim = dataFim }).ToList();
